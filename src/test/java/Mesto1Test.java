80928e053e3456bf6ba8e77c2a6812cdb062032c
Konstantin Yakovlev <yakovlev.kl@yandex.ru>
2022-01-11 14:57:43 +0500

    sprint #4 pom changed for allure reports

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -53,7 +53,7 @@ public class Mesto1Test {
         given()
                 .auth().oauth2(bearerToken) // Передаём токен для аутентификации
                 .put("/api/cards/{photoId}/likes", photoId) // Делаем PUT-запрос
-                .then().assertThat().statusCode(202); // Проверяем, что сервер вернул код 200
+                .then().assertThat().statusCode(200); // Проверяем, что сервер вернул код 200
     }
 
     @Step("Delete like from the photo by id")
